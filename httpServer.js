173c3a505e53b2e6e13ce79c845dab7f5889b7bf
Patrick Santos de Souza <patrickstsouza@gmail.com>
2018-05-05 22:33:02 +0100

    added points

--- httpServer.js
+++ httpServer.js
@@ -163,9 +163,16 @@ app.post('/uploadData', function (req, res) {
 			res.status(400).send(err);
 			return;
 		}
-		var querystring = "INSERT into formdata (name,surname,module,language, modulelist, lecturetime) values ('";
+
+
+		// pull the geometry component together
+		// note that well known text requires the points as longitude/latitude !
+		// well known text should look like: 'POINT(-71.064544 42.28787)'
+		var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")'";
+		var querystring = "INSERT into formdata (name,surname,module,language, modulelist, lecturetime, geom) values('";
 		querystring = querystring + req.body.name + "','" + req.body.surname + "','" + req.body.module + "','";
-		querystring = querystring + req.body.language + "','" + req.body.modulelist + "','"+ req.body.lecturetime + "')";
+		querystring = querystring + req.body.language + "','" + req.body.modulelist + "','" + req.body.lecturetime + "'," + geometrystring + "))";
+
 		console.log(querystring);
 		client.query(querystring, function (err, result) {
 			done();
